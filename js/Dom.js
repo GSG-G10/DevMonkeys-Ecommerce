3b0648e286f93c66184ca70f50b5168bd09b594d
NourAdawi <nouradwi12@gmail.com>
2021-07-14 17:50:48 +0300

    add item to cart in local storage

--- js/Dom.js
+++ js/Dom.js
@@ -1,3 +1,7 @@
+// const { getItem, addCard, storeData } = require("../js/data");
+
+
+storeData();
 let slideIndex = 0;
 showSlides();
 
@@ -14,3 +18,17 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+// Add to cart
+let cart = [];
+const addToCartButton = document.querySelectorAll(".add");
+addToCartButton.forEach((el) => {
+  console.log(el);
+  el.addEventListener("click", (e) => {
+    const item ={};
+    item.price= e.target.parentNode.children[2].textContent;
+    item.name= e.target.parentNode.children[0].children[0].textContent;
+
+    addCard(item)
+  });
+});
