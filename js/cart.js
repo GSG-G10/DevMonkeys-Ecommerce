9eb06a299a6b63c99b46c93449abed03028b59b0
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-07-14 19:30:57 +0300

    function remove from card and function get total price

--- js/cart.js
+++ js/cart.js
@@ -1,17 +1,19 @@
-
-
-const table = document.querySelector('table');
-const cartData = getItem();
-console.log(cartData);
-let tableHtml = "";
-cartData.forEach(element => {
-    tableHtml+= `  <tr>
+const table = document.querySelector("table");
+let cartData = getItem();
+// console.log(cartData);
+let tableHtml = `<tr>
+        <th>Product</th>
+        <th>Quantity</th>
+        <th>Price</th>
+        </tr>`;
+cartData.forEach((element) => {
+    tableHtml += `  <tr>
     <td>
         <div class="description-product">
             <div class="conatiner-image">
                 <img src=${element.image} alt="product1">
             </div>
-            <div class="container-content">
+            <div class="container-content" id=${element.id}>
                 <span>${element.name}</span>
                 <span>price : ${element.price}</span>
                 <span class="remove">remove</span>
@@ -19,9 +21,31 @@ cartData.forEach(element => {
         </div>
     </td>
     <td><input type="number" min="0"></td>
-    <td>20$</td>
- </tr> `
+    <td>${element.price}</td>
+ </tr> `;
 });
 
-table.innerHTML= tableHtml;
+table.innerHTML = tableHtml;
 
+const addToCartButton = document.querySelectorAll(".remove");
+addToCartButton.forEach((el) => {
+    console.log(el);
+    el.addEventListener("click", (e) => {
+        console.log(e.target.parentNode.parentNode.parentNode.parentNode);
+        removeItem(parseInt(e.target.parentNode.id));
+        const tbody = document.querySelector("tbody");
+        tbody.removeChild(e.target.parentNode.parentNode.parentNode.parentNode);
+        cartData = getItem();
+        getTotalPrice();
+    });
+});
+
+function getTotalPrice() {
+    const totalPrice = document.querySelector(".total-price");
+    totalPrice.innerHTML = cartData.reduce(
+        (total, current) => parseInt(current.price.substring(1)) + total,
+        0
+    );
+    console.log(totalPrice);
+}
+getTotalPrice();
--- js/data.js
+++ js/data.js
@@ -1,36 +1,10 @@
-const productsList = [
-  {
-    id: "p1",
-    name: "T-shirt",
-    imageUrl: "https://via.placeholder.com/150/255595/808080",
-    details: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
-    price: 25,
-    category: "cat_1",
-  },
-  {
-    id: "p2",
-    name: "Shouse",
-    imageUrl: "https://via.placeholder.com/150/255595/808080",
-    details: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
-    price: 15,
-    category: "cat_2",
-  },
-  {
-    id: "p3",
-    name: "Toy",
-    imageUrl: "https://via.placeholder.com/150/255595/808080",
-    details: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
-    price: 5,
-    category: "cat_3",
-  },
-];
-
 function storeData() {
   if (!localStorage.getItem("cart"))
     localStorage.setItem("cart", JSON.stringify([]));
 }
 function addCard(item) {
   const dataArray = JSON.parse(localStorage.getItem("cart"));
+  item.id = dataArray[dataArray.length - 1]?.id + 1 || 0;
   dataArray.push(item);
   localStorage.setItem("cart", JSON.stringify(dataArray));
 }
@@ -38,11 +12,11 @@ function getItem() {
   return JSON.parse(localStorage.getItem("cart"));
 }
 
-// if (typeof module !== "undefined") {
-//   module.exports = {
-//     products,
-//     getItem,
-//     addCard,
-//     storeData,
-//   };
-// }
+// function to remove item
+function removeItem(id) {
+  const dataArray = JSON.parse(localStorage.getItem('cart'));
+  const dataFiltered = dataArray.filter(function (item) {
+    return item.id != id;
+  });
+  localStorage.setItem('cart', JSON.stringify(dataFiltered));
+}
